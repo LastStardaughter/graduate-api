3352188c77f5e7c260767dfc5d342ef3d4634f4a
Scott Hackman <scottbert85@yahoo.com>
2020-02-18 12:10:31 -0500

    it works now

--- index.js
+++ index.js
@@ -20,7 +20,8 @@ mongoose.connect(connect)
   .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.error('Could not connect to MongoDB...'+err));
 
-app.use(express.json());
+//app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
 // app.use('/api/genres', genres);
 // app.use('/api/customers', customers);
 // app.use('/api/movies', movies);
